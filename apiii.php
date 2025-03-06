736d2b216186a2dd08aa0b4992153d348bd25568
badzxd <badzxdbind@gmail.com>
2025-03-06 09:09:22 +0700

    Update apiii.php

--- apiii.php
+++ apiii.php
@@ -2,7 +2,7 @@
 $subjek = $_POST['subjek'];
 $pesan = $_POST['pesan'];
 
-include 'panel/data.php';
+include 'panel.php/data.php';
 $sender = 'From: '.$nik.'<'.$sender.'>';
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
